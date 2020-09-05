566ab9e2716272c6c0af8f6ecbe6e252dfad085f
arsenbelarus <arsenbelarus@gmail.com>
2020-09-05 14:28:57 +0300

    form connected to heroku but workes only with localhost

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 import style from "./Contacts.module.css"
+import axios from "axios"
 
 type HeaderPropsType = {
 
@@ -7,15 +8,37 @@ type HeaderPropsType = {
 
 const Contacts = (props: HeaderPropsType) => {
 
+    const [inputValues, setInputValues] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        message: ""
+    })
+    const nameHandler = (e: ChangeEvent<HTMLInputElement>) => setInputValues({...inputValues, name: e.currentTarget.value})
+    const emailHandler = (e: ChangeEvent<HTMLInputElement>) => setInputValues({...inputValues, email: e.currentTarget.value})
+    const phoneHandler = (e: ChangeEvent<HTMLInputElement>) => setInputValues({...inputValues, phone: e.currentTarget.value})
+    const messageHandler = (e: ChangeEvent<HTMLTextAreaElement>) => setInputValues({...inputValues, message: e.currentTarget.value})
+
+    const submitHandler = (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        axios.post("https://arsen-mail-server.herokuapp.com/sendMessage", inputValues).then(() => {
+            alert("Your message has been sent")
+            setInputValues({...inputValues, name: "", message: "", email: "", phone: ""})
+        }).catch((err) => {
+            alert(err)
+        })
+    }
+
+
     return (
     <div className={style.contactsContainer}>
         <h2>Contacts</h2>
        <a id="contacts"/>
-        <form className={style.contactsForm}>
-            <input type="text" placeholder={"Name"}/>
-            <input type="text" placeholder={"E-mail"}/>
-            <input type="phone" placeholder={"Phone"}/>
-            <textarea name="" placeholder={"Your message"} rows={5}/>
+        <form className={style.contactsForm} onSubmit={submitHandler}>
+            <input type="text" placeholder={"Name"} value={inputValues.name} onChange={nameHandler}/>
+            <input type="email" placeholder={"E-mail"} value={inputValues.email} onChange={emailHandler}/>
+            <input type="tel" placeholder={"Phone"} value={inputValues.phone} onChange={phoneHandler}/>
+            <textarea name="" placeholder={"Your message"} rows={5} value={inputValues.message} onChange={messageHandler}/>
             <button>Submit</button>
         </form>
     </div>
