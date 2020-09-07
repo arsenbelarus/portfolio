2ab4fcde234d114ba631b638492bacc2efe93614
arsenbelarus <arsenbelarus@gmail.com>
2020-09-07 21:05:18 +0300

    react-scroll added

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -4,6 +4,8 @@ import SpecificSkill from "./specificSkill/SpecificSkill";
 import html from "../assets/logos/html.png";
 import css from "../assets/logos/css.png";
 import js from "../assets/logos/js.png";
+import react from "../assets/logos/react.png";
+import redux from "../assets/logos/redux.png";
 import {BlockHeader} from "../common/blockHeader/BlockHeader";
 
 type SkillsPropsType = {}
@@ -13,6 +15,8 @@ const Skills = (props: SkillsPropsType) => {
     const htmlBackground = {backgroundImage: `url(${html})`}
     const cssBackground = {backgroundImage: `url(${css})`}
     const jsBackground = {backgroundImage: `url(${js})`}
+    const reactBackground = {backgroundImage: `url(${react})`}
+    const reduxBackground = {backgroundImage: `url(${redux})`}
 
     return (
         <div className={`${style.skillsContainer} "skills"`}>
@@ -21,6 +25,8 @@ const Skills = (props: SkillsPropsType) => {
                 <SpecificSkill style={htmlBackground} skillName={"HTML"} skillDescription={"Bla Bla"}/>
                 <SpecificSkill style={cssBackground} skillName={"CSS"} skillDescription={"Bla Bla"}/>
                 <SpecificSkill style={jsBackground} skillName={"JavaScript"} skillDescription={"Bla Bla"}/>
+                <SpecificSkill style={reactBackground} skillName={"React"} skillDescription={"Bla Bla"}/>
+                <SpecificSkill style={reduxBackground} skillName={"Redux"} skillDescription={"Bla Bla"}/>
             </div>
         </div>
 
