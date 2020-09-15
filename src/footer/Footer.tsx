fcc840e1743bc8f6d1480aceefdbed6965aead24
arsenbelarus <arsenbelarus@gmail.com>
2020-09-15 09:07:02 +0300

    link's addresses changed

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import style from "./Footer.module.css"
 
-type FooterPropsType = {}
-
-const Footer = (props: FooterPropsType) => {
+const Footer = () => {
 
   return (
     <div className={style.footerContainer}>
       <div className={style.footerLinks}>
-        <a href="http://www.facebook.com">Facebook</a>
-        <a href="http://www.instagram.com">Instagram</a>
-        <a href="http://www.linkedin.com">LinkedIn</a>
-        <a href="http://www.vk.com">VK</a>
+        <a href="https://www.facebook.com/arsen.voskanyan.79">Facebook</a>
+        <a href="https://www.instagram.com/arsenvaskanian/">Instagram</a>
+        <a href="https://www.linkedin.com/in/arsen-vaskanian-291a9394">LinkedIn</a>
+        <a href="https://vk.com/arsen_voskanyan">VK</a>
       </div>
       <div className={style.footerCopyright}>
         @2020 all rights reserved
