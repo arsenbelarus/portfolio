0c9419f90ad97d79b96912c666ecc1d9760b2f2e
arsenbelarus <arsenbelarus@gmail.com>
2020-09-18 15:26:39 +0300

    upButton now is showed depending on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Header} from "./header/Header";
 import Main from "./main/Main";
 import Skills from "./skills/Skills";
@@ -7,17 +7,28 @@ import Contacts from "./contacts/Contacts";
 import Footer from "./footer/Footer";
 import {UpButton} from "./upButton/UpButton";
 
-const App = () =>
-    (
-        <div className="main">
-            <Header/>
-            <Main/>
-            <Skills/>
-            <Portfolio/>
-            <Contacts/>
-            <Footer/>
-            <UpButton/>
-        </div>
-    )
+const App = () => {
+  const [isTop, setIsTop] = useState(false)
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      const position = window.scrollY > 400
+      if (isTop !== position) {
+        setIsTop(position)
+      }
+    })
+  }, [isTop])
+
+  return (
+    <div className="main">
+      <Header/>
+      <Main/>
+      <Skills/>
+      <Portfolio/>
+      <Contacts/>
+      <Footer/>
+      {isTop ? <UpButton/> : null}
+    </div>
+  )
+}
 
 export default App;
